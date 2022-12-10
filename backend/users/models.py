2586b387f1432629fea5b455ddfbf7d51591679e
Sergey Mankevich <serg.mankevi@yandex.ru>
2022-12-10 17:21:12 +0300

    Added MyUser model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,44 @@
+"""Модели приложения users для управления пользователями.
+Модель пользователя (user) основана на AbstractUser из Django.
+"""
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class MyUser(AbstractUser):
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        max_length=254,
+        unique=True,
+    )
+    username = models.CharField(
+        verbose_name='Уникальный логин',
+        max_length=150,
+        unique=True,
+    )
+    first_name = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=150,
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия пользователя',
+        max_length=150,
+    )
+    password = models.CharField(
+        verbose_name='Пароль',
+        max_length=150,
+    )
+    subscribe = models.ManyToManyField(
+        'self',
+        verbose_name='Подписка',
+        related_name='subscribes',
+        symmetrical=False,
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь',
+        verbose_name_plural = 'Пользователи',
+        ordering = ('username', )
+
+    def __str__(self):
+        return f'Логин: {self.username}, почта: {self.email}'
