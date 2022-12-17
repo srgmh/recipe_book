f590681c7daa77bb7e740155cf9b0c9ac643a365
Sergey Mankevich <serg.mankevi@yandex.ru>
2022-12-17 13:56:17 +0300

    Workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
