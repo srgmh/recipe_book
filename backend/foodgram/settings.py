0f77e0893f23a5eb033f85f25b5addf8264dd42c
Sergey Mankevich <serg.mankevi@yandex.ru>
2022-12-19 14:03:07 +0300

    Deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-DEBUG = os.environ.get('DJANGO_DEBUG', default=False)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
